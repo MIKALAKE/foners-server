66f1a2262cf9db4fc23837f49db36966444fa351
mikalake <abrahamwashington300@gmail.com>
2022-06-21 17:01:10 +0300

    Constructor endpoints and seeds added

--- app/controllers/v1/constructors_controller.rb
+++ app/controllers/v1/constructors_controller.rb
@@ -11,5 +11,31 @@ module V1
       constructor = Constructor.find(params[:id])
       render json: constructor
     end
+
+    def create
+      constructor = Constructor.new(constructor_params)
+      constructor.save! if constructor.valid?
+      render json: constructor ? constructor : { msg: "none" }
+    end
+
+    def update
+      @constructor.update(constructor_params)
+      render json: @constructor
+    end
+
+    def destroy
+      @constructor.destroy
+      render json: { msg: "Deleted" }
+    end
+
+    protected
+
+    def constructor_params
+      params.permit(:cover_url, :description, :first_apparence, :logo_url, :name, :origins, :titles)
+    end
+
+    def find_constructor
+      @constructor ||= Constructor.find(params[:id])
+    end
   end
 end
