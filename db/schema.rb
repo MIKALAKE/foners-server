1d30f696b956f21f75c88ce2ea590f1ebc65bcdd
mikalake <abrahamwashington300@gmail.com>
2022-07-08 11:36:32 +0300

    Seeds for events added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_145451) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_081331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_145451) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "city"
-    t.date "date"
+    t.string "date"
     t.string "cover_url"
     t.string "description"
     t.string "country"
